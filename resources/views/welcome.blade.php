ed554a99e2017f82451f4909afde1bccbbba1215
gustavo <gasantaella@gmail.com>
2021-06-04 14:33:15 -0400

    push heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,11 @@
 @extends("layout/layoutMaster")
+{{-- @section('css')
+<style type="text/css">
+    a{
+  scroll-behavior: smooth!important;
+}
+</style>
+@endsection --}}
 @section("title", "Inicio")
 @section("content")
 @include("inc/about") 
